a1968a7e8903d185898c9b65d2e96f28aac8c6ac
Marcellus Tavares <marcellus.tavares@liferay.com>
2016-10-29 10:13:54 -0300

    #3 Increase the frequency of open code reviews job

--- dashing/jobs/code_reviews.rb
+++ dashing/jobs/code_reviews.rb
@@ -1,8 +1,6 @@
-require 'net/http'
-require 'json'
-require 'time'
-require 'open-uri'
-require 'cgi'
+require 'octokit'
+
+$client = Octokit::Client.new(:access_token => ENV["GITHUB_CLIENT_TOKEN"])
 
 code_reviewers = {
   "brandizzi" => {label: "Adam"},
@@ -16,30 +14,19 @@ code_reviewers = {
 }
 
 def photo_user_profile(owner)
-  uri = URI.parse("https://api.github.com/users/#{owner}")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = uri.scheme == 'https'  
-  request = Net::HTTP::Get.new(uri.request_uri)
-  user = JSON.parse(http.request(request).body)  
-  avatar_url = user['avatar_url']
+  user = $client.user owner
+  user['avatar_url']
 end
 
 code_reviewers.each {|owner, mapping| mapping['avatar_url'] = photo_user_profile(owner)}
 
 def number_of_open_pull_requests(owner)
-  uri = URI.parse("https://api.github.com/repos/#{owner}/liferay-portal/pulls")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = uri.scheme == 'https'
-  puts uri.request_uri
-  request = Net::HTTP::Get.new(uri.request_uri)
-  pulls = JSON.parse(http.request(request).body)
-  pulls.count
+  open_pull_requets = $client.pull_requests('%{owner}/liferay-portal' % {owner: owner}, :state => 'open')
+  open_pull_requets.count
 end
 
-SCHEDULER.every '10m' do
+SCHEDULER.every '5m' do
   code_reviewers.each {|owner, mapping| mapping['value'] = number_of_open_pull_requests(owner)}
-
-  puts code_reviewers
 
   send_event('code-reviews', { items: code_reviewers.values })
 end
