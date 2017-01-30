4f016fa503c86a44db501f1d6dbba8b3ec81620f
Inacio Nery <inacio.nery@liferay.com>
2017-01-30 09:30:36 -0300

    #7 Automatically capture sprint id

--- dashing/jobs/jira_sprint.rb
+++ dashing/jobs/jira_sprint.rb
@@ -7,6 +7,30 @@ require_relative '../conf/dashboard_config'
 
 JIRA_SPRINT_CONFIG = DashboardConfig.load()
 
+def get_sprint_id(url, username, password, rapid_view_id)
+  uri = URI.parse("#{url}/rest/agile/1.0/board/#{rapid_view_id}/sprint")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = uri.scheme == 'https'
+  request = Net::HTTP::Get.new(uri.request_uri)
+
+  if !username.nil? && !username.empty?
+    request.basic_auth(username, password)
+  end
+
+  data = JSON.parse(http.request(request).body)
+
+  id = 0
+
+  data["values"].each do |sprint| 
+      if(sprint["state"] == "active")
+        id = sprint["id"]
+        break
+      end
+  end
+
+  id
+end
+
 def get_sprint_statistics(url, username, password, rapid_view_id, sprint_id)
   uri = URI.parse("#{url}/rest/greenhopper/1.0/rapid/charts/sprintreport?rapidViewId=#{rapid_view_id}&sprintId=#{sprint_id}")
   http = Net::HTTP.new(uri.host, uri.port)
@@ -59,13 +83,17 @@ def is_wip_issue?(issue)
 end
 
 SCHEDULER.every '1h', :first_in => 0 do
-  forms_print_statistics = get_sprint_statistics(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:forms_rapid_view_id], JIRA_SPRINT_CONFIG[:forms_sprint_id])
-  workflow_sprint_statistics = get_sprint_statistics(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:workflow_rapid_view_id], JIRA_SPRINT_CONFIG[:workflow_sprint_id])
-  
+  forms_sprint_id = get_sprint_id(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:forms_rapid_view_id])
+  forms_print_statistics = get_sprint_statistics(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:forms_rapid_view_id], forms_sprint_id)
 
   send_event('forms-sprint-days-remaining', {current: forms_print_statistics['days_remaining']})
   send_event('forms-sprint-progress', {value: forms_print_statistics['progress']})
+
+  workflow_sprint_id = get_sprint_id(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:workflow_rapid_view_id])
+  workflow_sprint_statistics = get_sprint_statistics(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:workflow_rapid_view_id], workflow_sprint_id)
+
   send_event('workflow-sprint-days-remaining', {current: workflow_sprint_statistics['days_remaining']})
   send_event('workflow-sprint-progress', {value: workflow_sprint_statistics['progress']})
+
   send_event('wip-tasks', {tasks: forms_print_statistics['wip_issues'] + workflow_sprint_statistics['wip_issues']})
 end
