8469da75a1cb2ab46335782e7b2cbaee39dd25cc
Inacio Nery <inacio.nery@liferay.com>
2017-06-30 13:28:41 -0300

    Show work in progress by components

--- dashing/jobs/jira_sprint.rb
+++ dashing/jobs/jira_sprint.rb
@@ -8,8 +8,8 @@ require_relative '../conf/dashboard_config'
 
 JIRA_SPRINT_CONFIG = DashboardConfig.load()
 
-def get_sprint_id(url, username, password, rapid_view_id)
-  uri = URI.parse("#{url}/rest/agile/1.0/board/#{rapid_view_id}/sprint")
+def forms_get_in_progress(url, username, password)
+  uri = URI.parse("#{url}/rest/api/2/search?jql=component%20in%20(Forms%2C%20subcomponents(LPS%2C%20%27Dynamic%20Data%20Lists%27%2C%20%27true%27)%2C%20subcomponents(LPS%2C%20%27Web%20Form%27%2C%20%27true%27)%2C%20subcomponents(LPS%2C%20%27Dynamic%20Data%20Mapping%27%2C%20%27true%27)%2C%20subcomponents(LPS%2C%20Polls%2C%20%27true%27))%20and%20status%20%3D%20%27In%20Progress%27%20%20and%20project%20%3D%20'PUBLIC%20-%20Liferay%20Portal%20Community%20Edition'%20and%20status%20%3D%20'In%20Progress'%20%20and%20project%20%3D%20%22PUBLIC%20-%20Liferay%20Portal%20Community%20Edition%22%20&fields=key,summary,assignee,status")
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = uri.scheme == 'https'
   request = Net::HTTP::Get.new(uri.request_uri)
@@ -20,91 +20,56 @@ def get_sprint_id(url, username, password, rapid_view_id)
 
   data = JSON.parse(http.request(request).body)
 
-  id = 0
+  sprint_statistics = {}
 
-  data["values"].each do |sprint| 
-      if(sprint["state"] == "active" && sprint["originBoardId"] == rapid_view_id)
-        id = sprint["id"]
-        break
-      end
-  end
+  wip_issues = []
 
-  id
-end
+  data["issues"].each {|issue| wip_issues.push({'title' => get_issue_title(issue), 'assignee' => issue["fields"]["assignee"]["displayName"]})}
 
-def get_sprint_statistics(url, username, password, rapid_view_id, sprint_id)
-  uri = URI.parse("#{url}/rest/greenhopper/1.0/rapid/charts/sprintreport?rapidViewId=#{rapid_view_id}&sprintId=#{sprint_id}")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.use_ssl = uri.scheme == 'https'
-  request = Net::HTTP::Get.new(uri.request_uri)
-
-  if !username.nil? && !username.empty?
-    request.basic_auth(username, password)
-  end
-
-  data = JSON.parse(http.request(request).body)
-
-  sprint_data = data["sprint"]
-  contents_data = data["contents"]
-
-  sprint_statistics = {"days_remaining" => 0, "progress" => 0, "wip_issues" => 0}
-
-  endDate = Date.parse(sprint_data["endDate"])
-  today = Date.today
-
-  sprint_statistics['days_remaining'] = endDate.mjd - today.mjd
-  sprint_statistics['progress'] = calculate_sprint_progress(contents_data["completedIssues"].count, contents_data["issuesCompletedInAnotherSprint"].count, contents_data["issuesNotCompletedInCurrentSprint"].count)
-  sprint_statistics['wip_issues'] = get_wip_issues(contents_data["issuesNotCompletedInCurrentSprint"])
+  sprint_statistics['wip_issues'] = wip_issues
 
   sprint_statistics
 end
 
-def calculate_sprint_progress(completed_issues_count, completed_issues_from_another_sprint_count ,not_completed_issues_count)
-  total = completed_issues_count + completed_issues_from_another_sprint_count + not_completed_issues_count
-  progress = ((completed_issues_count.to_f + completed_issues_from_another_sprint_count.to_f) / total) * 100
-  progress.to_i
-end
+def workflow_get_in_progress(url, username, password)
+  uri = URI.parse("#{url}/rest/api/2/search?jql=component%20in%20(subcomponents(LPS%2C%20Workflow%2C%20%27true%27)%2C%20subcomponents(LPS%2C%20%27Business%20Productivity%27%2C%20%27true%27)%2C%20subcomponents(LPS%2C%20Calendar%2C%20%27true%27))%20and%20status%20%3D%20'In%20Progress'%20%20and%20project%20%3D%20%22PUBLIC%20-%20Liferay%20Portal%20Community%20Edition%22%20&fields=key,summary,assignee,status")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = uri.scheme == 'https'
+  request = Net::HTTP::Get.new(uri.request_uri)
 
-def get_wip_issues(not_completed_issues)
+  if !username.nil? && !username.empty?
+    request.basic_auth(username, password)
+  end
+
+  data = JSON.parse(http.request(request).body)
+
+  sprint_statistics = {}
+
   wip_issues = []
-  not_completed_issues.each {|issue| wip_issues.push({'title' => get_issue_title(issue), 'assignee' => issue["assigneeName"]}) if is_wip_issue?(issue)}
 
-  wip_issues
+  data["issues"].each {|issue| wip_issues.push({'title' => get_issue_title(issue), 'assignee' => issue["fields"]["assignee"]["displayName"]})}
+
+  sprint_statistics['wip_issues'] = wip_issues
+
+  sprint_statistics
 end
 
 def get_issue_title(issue)
-  title = issue["key"] + " " + issue["summary"][0..40]
+  title = issue["key"] + " " + issue["fields"]["summary"][0..40]
 
-  if (issue["summary"].length > 40)
+  if (issue["fields"]["summary"].length > 40)
     title += "..."
   end
 
   title
 end
 
-def is_wip_issue?(issue)
-  status_name = issue["statusName"]
+SCHEDULER.every '15m', :first_in => 0 do
+  forms_get_in_progress = forms_get_in_progress(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"])
 
-  if status_name.eql? "In Development" or status_name.eql? "In Progress"
-    true
-  else
-    false
-  end
-end
+  send_event('forms-wip-tasks', {tasks: forms_get_in_progress['wip_issues']})
 
-SCHEDULER.every '1h', :first_in => 0 do
-  forms_sprint_id = get_sprint_id(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:forms_rapid_view_id])
-  forms_print_statistics = get_sprint_statistics(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:forms_rapid_view_id], forms_sprint_id)
+  workflow_get_in_progress = workflow_get_in_progress(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"])
 
-  send_event('forms-sprint-days-remaining', {current: forms_print_statistics['days_remaining']})
-  send_event('forms-sprint-progress', {value: forms_print_statistics['progress']})
-  send_event('forms-wip-tasks', {tasks: forms_print_statistics['wip_issues']})
-
-  workflow_sprint_id = get_sprint_id(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:workflow_rapid_view_id])
-  workflow_sprint_statistics = get_sprint_statistics(JIRA_SPRINT_CONFIG[:jira_url], ENV["JIRA_USERNAME"], ENV["JIRA_PASSWORD"], JIRA_SPRINT_CONFIG[:workflow_rapid_view_id], workflow_sprint_id)
-
-  send_event('workflow-sprint-days-remaining', {current: workflow_sprint_statistics['days_remaining']})
-  send_event('workflow-sprint-progress', {value: workflow_sprint_statistics['progress']})
-  send_event('workflow-wip-tasks', {tasks: workflow_sprint_statistics['wip_issues']})
-
+  send_event('workflow-wip-tasks', {tasks: workflow_get_in_progress['wip_issues']})
 end
